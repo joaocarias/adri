7db06d27dd8142d5b74ef739abdb5e5d0b80613f
João Carias <joaocariasdefranca@gmail.com>
2018-07-31 14:20:26 -0300

    modificando para https

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 session_start();
     
     if(!(isset($_SESSION['logado'])) OR  $_SESSION['logado'] != '1'){
-        header("location: page/login.php");
+        header("location: https://natal.rn.gov.br/sms/remanejamentointerno/page/login.php");
     }else{
-        header("location: page/dashboard.php");
+        header("location: https://natal.rn.gov.br/sms/remanejamentointerno/page/dashboard.php");
     }
     
     
